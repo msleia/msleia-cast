2a7215db9939a0f99c10c7436c48a872693b0f7a
Mayoor Rao <mayoor.rao@gmail.com>
2018-11-16 17:36:53 -0800

    display vocab

--- src/Controller.js
+++ src/Controller.js
@@ -14,37 +14,50 @@ class Controller extends Component{
         command : "WELCOME",
         message : "Hi! This is Leia. What would you like to Learn?",
         prevMsg : "",
-        feedback: ""
+        feedback: "",
+        mastered_content: []
     };
     handleCommand(payload){
         console.log("Entering handle command");
         const commandMsg = payload.command;
         const special_icon = payload.feedback;
-        
-        console.log("new command is: "+commandMsg.toUpperCase()+". Old Command was: "+this.state.command.toUpperCase());
-        if (commandMsg.toUpperCase() !== "WELCOME" && commandMsg.toUpperCase() !== "INIT"){
-            if (this.state.command.toUpperCase() !== "WELCOME" && this.state.command.toUpperCase() !== "INIT"){
-                this.setState({prevMsg:this.state.command});
-                this.setState({feedback:special_icon});
-                console.log("setting prev message to: "+this.state.prevMsg+" and feedback icon to "+this.state.feedback);
-            }
-            setTimeout(() => {
-                this.setState({message:commandMsg});
-                this.setState({feedback:""})
-            }, 2000);
-            this.setState({command:"COMM"});
-            
+        console.log(typeof(commandMsg))
+        if (commandMsg instanceof Object && 'mastered_vocab' in commandMsg) {
+            this.setState({command:'mastered_vocab'});
+            this.setState({mastered_content:commandMsg.mastered_vocab})
         } else {
-            this.setState({command:commandMsg});
+            console.log("new command is: "+commandMsg.toUpperCase()+". Old Command was: "+this.state.command.toUpperCase());
+            if (commandMsg.toUpperCase() !== "WELCOME" && commandMsg.toUpperCase() !== "INIT"){
+                if (this.state.command.toUpperCase() !== "WELCOME" && this.state.command.toUpperCase() !== "INIT"){
+                    this.setState({prevMsg:this.state.command});
+                    this.setState({feedback:special_icon});
+                    console.log("setting prev message to: "+this.state.prevMsg+" and feedback icon to "+this.state.feedback);
+                }
+                setTimeout(() => {
+                    this.setState({message:commandMsg});
+                    this.setState({feedback:""})
+                }, 2000);
+                this.setState({command:"COMM"});
+                
+            } else {
+                this.setState({command:commandMsg});
+            }
         }
     }
     render(){
+
+        var message = this.state.message
         
+        if (commandMsg == 'mastered_vocab') {
+            message = <ul>this.state.mastered_content.map( (w)=> <li>w.word</li>)</ul>
+        } else {
+            message = <Welcome message={this.state.message} icon={this.state.feedback}/>
+        }
         
         return (
             <div class="container">
                 <WebSocket commandHandler={this.handleCommand}/>
-                <Welcome message={this.state.message} icon={this.state.feedback}/>
+                {message}
             </div>
         )
         
